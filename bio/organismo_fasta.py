0ff8249dfe255501d69eeb04478efeaca02294d3
Natália Gaeta <natalia.gaeta@hotmail.com>
2024-08-02 14:28:58 -0300

    atualização

--- bio/organismo_fasta.py
+++ bio/organismo_fasta.py
@@ -2,8 +2,8 @@
 from bio.sequencia import Sequencia
 
 class OrganismoFasta:
-    def __init__(self, id, nome, Sequencia):
+    def __init__(self, id, nome, sequencia):
         self.id = id
         self.nome = nome
-        self.sequencia = Sequencia
+        self.sequencia = Sequencia(sequencia)
         
